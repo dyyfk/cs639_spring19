17a143966673b846a983942b594890b2560f4871
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-02-28 11:45:35 -0600

    doing ex2

--- assignments/assignment3/join.py
+++ assignments/assignment3/join.py
@@ -11,12 +11,22 @@ mr = MapReduce.MapReduce()
 # Do not modify above this line
 
 # Implement the MAP function
-def mapper(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def mapper(rec):
+  mr.emit_intermediate(rec[1], rec)
 
 # Implement the REDUCE function
-def reducer(# Provide the necessary inputs):
-    # YOUR CODE GOES HERE
+def reducer(key, list_of_values):
+  a = []
+  for i in list_of_values:
+    if(i[0]=='line_item'):
+      
+
+      
+    else:
+      a.append(i)
+
+    # for j in i:
+    # mr.emit(a) 
 
 # Do not modify below this line
 # =============================
